7799c4bbd65e6b93af1cd9e751c73ce573969ac2
srinich72-git <srinich72@gmail.com>
2026-06-16 22:23:55 +0530

    Update S3 bucket and region in backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-2048-reyaz-4" # Replace with your actual S3 bucket name
+    bucket = "devsecops-2048-srinich72" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
